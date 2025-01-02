96e4e953d4df2fbac89b935ff9726444744fb684
Madalin Bogdea <madalinbogdea@icloud.com>
2025-01-02 22:28:32 +0200

    add new algorithm

---
+++ y1s1/evenOddChecker.js
@@ -0,0 +1,11 @@
+function checkEvenOdd(number) {
+  if (number % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
+
+// example usage:
+console.log(checkEvenOdd(2));
+console.log(checkEvenOdd(5));
